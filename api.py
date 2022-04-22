1918814f25367dabfddac0de2c54625e59fb6fe0
Thibault <tbmc33@gmail.com>
2022-04-22 17:23:04 +0200

    Initial commit

---
+++ api.py
@@ -0,0 +1,13 @@
+﻿from flask import Flask
+from video_capture import read
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_image():
+    read()
+    return "test"
+
+
+
---
+++ video_capture.py
@@ -0,0 +1,16 @@
+﻿import cv2 as cv
+
+video_capture = cv.VideoCapture(0)
+opened = video_capture.isOpened()
+
+if not opened:
+    print("Cannot open stream")
+    exit(1)
+
+
+def read():
+    _, image = video_capture.read()
+    cv.imshow("Video", image)
+    cv.waitKey()
+    cv.destroyAllWindows()
+
